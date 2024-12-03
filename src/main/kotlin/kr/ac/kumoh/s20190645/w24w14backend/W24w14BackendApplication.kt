b9ca387bc2fc74344491901d7387a554c02351e1
sss2013 <kshi9478@gmail.com>
2024-12-03 17:42:05 +0900

    DB 연결

---
+++ src/main/kotlin/kr/ac/kumoh/s20190645/w24w14backend/W24w14BackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20190645.w24w14backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24w14BackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<W24w14BackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20190645/w24w14backend/W24w14BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20190645.w24w14backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w14BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
