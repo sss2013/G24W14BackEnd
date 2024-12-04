80d4eff6c9683dd876b0d89d44e5615c491483e4
sss2013 <kshi9478@gmail.com>
2024-12-04 16:15:21 +0900

    Backend 완성

--- src/main/kotlin/kr/ac/kumoh/s20190645/w24w14backend/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20190645/w24w14backend/controller/SongController.kt
@@ -1,12 +1,32 @@
 package kr.ac.kumoh.s20190645.w24w14backend.controller
 
 import kr.ac.kumoh.s20190645.w24w14backend.model.Song
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 import kr.ac.kumoh.s20190645.w24w14backend.service.SongService
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api/songs")
 class SongController(private val service: SongService) {
-    @GetMapping("/api/songs")
-    fun getAllSongs():List<Song> = service.getAllSongs()
+    @PostMapping
+    fun addSong(@RequestBody song: Song): Song = service.addSong(song)
+
+    @GetMapping
+    fun getAllSongs(): List<Song> = service.getAllSongs()
+
+    @GetMapping("/{id}")
+    fun getSongById(@PathVariable id: String): Song? = service.getSongById(id)
+
+    @GetMapping("/singer/{singer}")
+    fun getSongBySinger(@PathVariable singer: String): List<Song> = service.getSongBySinger(singer)
+
+    @PutMapping("/{id}")
+    fun updateSong(@PathVariable id: String, @RequestBody songDetails: Song): Song? =  service.updateSong(id, songDetails)
+
+    @DeleteMapping("/{id}")
+    fun deleteSong(@PathVariable id: String): Map<String, String> {
+        return if (service.deleteSong(id))
+            mapOf("status" to "deleted")
+        else
+            mapOf("status" to "not found")
+    }
 }
--- src/main/kotlin/kr/ac/kumoh/s20190645/w24w14backend/service/SongService.kt
+++ src/main/kotlin/kr/ac/kumoh/s20190645/w24w14backend/service/SongService.kt
@@ -5,6 +5,37 @@ import org.springframework.stereotype.Service
 import kr.ac.kumoh.s20190645.w24w14backend.repository.SongRepository
 
 @Service
-class SongService(private val songRepository: SongRepository) {
-    fun getAllSongs(): List<Song> = songRepository.findAll()
+class SongService(private val repository: SongRepository) {
+    fun addSong(song: Song): Song = repository.save(song)
+
+    fun getAllSongs(): List<Song> = repository.findAll()
+    fun getSongById(id: String): Song? = repository.findById(id).orElse(null)
+    fun getSongBySinger(title: String): List<Song> = repository.findBySinger(title)
+
+    fun updateSong(id: String, song: Song): Song? {
+        val songTarget = repository.findById(id)
+
+        return if (songTarget.isPresent) {
+            val oldSong = songTarget.get()
+            val updatedSong = oldSong.copy(
+                title = song.title,
+                singer = song.singer,
+                rating = song.rating,
+                lyrics = song.lyrics
+            )
+            repository.save(updatedSong)
+        } else {
+            null
+        }
+    }
+
+    fun deleteSong(id: String): Boolean {
+        return if (repository.existsById(id)) {
+            repository.deleteById(id)
+            true
+        } else {
+            false
+        }
+    }
+
 }
